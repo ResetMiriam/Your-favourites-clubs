8f5ea457e2cb39f59147afcb41f1d9fbbcbf3aa8
MiriamVF <miriam.verduf@gmail.com>
2021-10-05 12:57:27 +0200

    add filter

--- src/components/App.js
+++ src/components/App.js
@@ -7,18 +7,21 @@ function App() {
   const [newName, setNewName] = useState("");
   const [newWeekday, setWeekday] = useState(false);
   const [newWeekend, setWeekend] = useState(false);
+  const [filter, setFilter] = useState("all");
 
   const handleNewName = (ev) => {
-    setNewName(ev.currentTarget.value);
+    setNewName(ev.target.value);
   };
 
   const handleWeekday = (ev) => {
-    setWeekday(ev.currentTarget.checked);
-    console.log(setWeekday);
+    setWeekday(ev.target.checked);
   };
 
   const handleWeekend = (ev) => {
-    setWeekend(ev.currentTarget.checked);
+    setWeekend(ev.target.checked);
+  };
+  const handleFilter = (ev) => {
+    setFilter(ev.target.value);
   };
 
   const handleClick = (ev) => {
@@ -32,27 +35,47 @@ function App() {
     setData([...data, newClub]);
   };
 
-  const htmlClubList = data.map((oneClub, index) => (
-    <li key={index} className="club__item">
-      <p className="club__name">
-        #{index}: {oneClub.name}
-      </p>
-      <p className="club__weekday">
-        Abierto entre semana: {oneClub.openOnWeekdays ? "Sí" : "No"}
-      </p>
-      <p className="club__weekend">
-        Abierto el fin de semana: {oneClub.openOnWeekend ? "Sí" : "No"}
-      </p>
-    </li>
-  ));
-
+  const htmlClubList = () => {
+    return data
+      .filter((oneClub) => {
+        if (filter === "openOnWeekDays") {
+          return oneClub.openOnWeekdays === true;
+        } else if (filter === "openOnWeekends") {
+          return oneClub.openOnWeekend === true;
+        }
+        return true;
+      })
+      .map((oneClub, index) => {
+        return (
+          <li key={index} className="club__item">
+            <p className="club__name">
+              #{index}: {oneClub.name}
+            </p>
+            <p className="club__weekday">
+              Abierto entre semana: {oneClub.openOnWeekdays ? "Sí" : "No"}
+            </p>
+            <p className="club__weekend">
+              Abierto el fin de semana: {oneClub.openOnWeekend ? "Sí" : "No"}
+            </p>
+            <button>X</button>
+          </li>
+        );
+      });
+  };
   return (
     <>
       <header className="header">
         <h1 className="header__title">Mis Clubs</h1>
+        <form action="">
+          <select value={filter} onChange={handleFilter}>
+            <option value="all">Todos</option>
+            <option value="openOnWeekDays">Abre entre semana</option>
+            <option value="openOnWeekends">Abre los fines de semana</option>
+          </select>
+        </form>
       </header>
       <main>
-        <ul className="club__list">{htmlClubList}</ul>
+        <ul className="club__list">{htmlClubList()}</ul>
         <form className="new-club__form">
           <h2 className="new-club__title">Añade un nuevo club</h2>
           <label htmlFor="name"> Nombre del club</label>
